d0636ea07b46add1c367142dab4d092f809015b1
gabofc <gabofc@gmail.com>
2019-10-10 10:01:03 -0500

    adjust pc aligment

--- js/script.js
+++ js/script.js
@@ -159,8 +159,6 @@ function calculaSize() {
 		textArea = contenidoSize * 0.25;
 		textoRueda = ( textoRueda > 450 ) ? 450 : textoRueda;
 		contenidoSize = ( contenidoSize > 320 ) ? 320 : contenidoSize;
-		wheel.css( { 'width': ruedaSize, 'height': ruedaSize } );
-		$( '.contenidoPrincipal' ).css( { 'width': ruedaSize, 'height': ruedaSize } );
 		var minimoTop = 100 + logoSize;
 		$( '.dLogo' ).css( 'width', '100%' );
 		if ( ruedaSize > anchoVentana ) {
@@ -192,6 +190,7 @@ function calculaSize() {
 		}
 		textArea = contenidoSize * 0.25;
 		textoRueda = contenidoSize + 120;
+		textoRueda = ( ruedaSize < textoRueda ) ? ruedaSize : textoRueda;
 		if ( ventanaSize < 980 ) {
 			textoRueda = ( textoRueda > 650 ) ? 650 : textoRueda;
 		}
