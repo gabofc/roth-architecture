6b6c1ee9137dfb20538149f18a1cb2c9d942daeb
G@bo <gfernandez@junkyard.mx>
2019-10-11 15:08:53 -0500

    gif, png updated for animations

--- lib/icons.php
+++ lib/icons.php
@@ -1,16 +1,16 @@
 <?php
 	$iconsLinks = array(
-		array( 'Kin Toh', '01_kin_toh.svg', '01_kin_toh.gif', '#' ),
-		array( 'Cenote', '02_cenote.svg', '02_cenote.gif', '#' ),
-		array( 'Tseen Ja', '03_tseen_ja.svg', '03_tseen_ja.gif', '#' ),
-		array( 'Zak Ik', '04_zak_ik.svg', '04_zak_ik.gif', '#' ),
-		array( 'Sfer Ik in Tulum', '05_sfer_ik_tulum.svg', '05_sfer_ik_tulum.gif', '#' ),
-		array( 'Ik Raum', '06_ik_raum.svg', '06_ik_raum.gif', '#' ),
-		array( 'Sfer Ik in Uh May', '07_sfer_ik_uh_may.svg', '07_sfer_ik_uh_may.gif', '#' ),
-		array( 'Roth\'s House', '08_roths_house.svg', '08_roths_house.gif', '#' ),
-		array( 'Zak Ik Jewelry', '09_zak_ik_jewelry.svg', '09_zak_ik_jewelry.gif', '#' ),
-		array( 'Azulik Cuisine', '10_azulik_cuisine.svg', '10_azulik_cuisine.gif', '#' ),
-		array( 'Azulik Hotel', '11_azulik_hotel.svg', '11_azulik_hotel.gif', '#' ),
+		array( 'Kin Toh', '01_kin_toh.png', '01_kin_toh.gif', '#' ),
+		array( 'Cenote', '02_cenote.png', '02_cenote.gif', '#' ),
+		array( 'Tseen Ja', '03_tseen_ja.png', '03_tseen_ja.gif', '#' ),
+		array( 'Zak Ik', '04_zak_ik.png', '04_zak_ik.gif', '#' ),
+		array( 'Sfer Ik in Tulum', '05_sfer_ik_tulum.png', '05_sfer_ik_tulum.gif', '#' ),
+		array( 'Ik Raum', '06_ik_raum.png', '06_ik_raum.gif', '#' ),
+		array( 'Sfer Ik in Uh May', '07_sfer_ik_uh_may.png', '07_sfer_ik_uh_may.gif', '#' ),
+		array( 'Roth\'s House', '08_roths_house.png', '08_roths_house.gif', '#' ),
+		array( 'Zak Ik Jewelry', '09_zak_ik_jewelry.png', '09_zak_ik_jewelry.gif', '#' ),
+		array( 'Azulik Cuisine', '10_azulik_cuisine.png', '10_azulik_cuisine.gif', '#' ),
+		array( 'Azulik Hotel', '11_azulik_hotel.png', '11_azulik_hotel.gif', '#' ),
 		array( 'Maya Spa', '12_maya_spa.svg', '12_maya_spa.gif', '#' ),
 	);
 ?>
