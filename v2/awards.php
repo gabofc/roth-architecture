a2e3400c99a10974bc5694707aa379e055020bd5
G@bo <gabofc@gmail.com>
2020-10-15 16:28:16 -0500

    Awards update

--- v2/awards.php
+++ v2/awards.php
@@ -33,6 +33,9 @@
 					<b class="scrollAnimation">WINNER</b>
 					<b class="scrollAnimation">Category: Details Plus Architecture + Wood</b>
 					<b class="scrollAnimation">Project: SFER IK Museion Uh May (Jury Winner)</b>
+						<b class="scrollAnimation">WINNER</b>
+						<b class="scrollAnimation">Category: Details Plus Architecture + Wood / Residential Private House (XL > 500 sq. ft.) </b>
+						<b class="scrollAnimation">Project: SFER IK Museion Uh May (Jury Winner) / AZULIK Residence (Special Mention)</b>
 					<span class="scrollAnimation"></span>
 					<p class="scrollAnimation">The Architizer A+Awards is the largest awards program focused on promoting and celebrating the year’s best architecture and products. The awards bring together the largest online community of architects in the world, including the elite of world architecture, who champion meaningful design and the qualities that are integral to bringing great buildings to life.</p>
 				</div>
@@ -80,6 +83,48 @@
 			<div class="awardImg scrollAnimation">
 				<img src="images/awards/4.jpg">
 				 <a href="http://www.interiordesign.net/award/9-best-of-year-awards-winners-2019/#Small-Museum-Art-Gallery" target="_blank">Read More</a>
+				<div class="linea"></div>
+			</div>
+		</div>
+
+		<div class="awardItem">
+			<div class="contenido">
+				<div class="titulos">
+					<h3 class="scrollAnimation">Frame Awards</h3>
+					<h4 class="scrollAnimation">Amsterdam, Netherlands.</h4>
+				</div>
+				<div class="descripcion">
+					<b class="scrollAnimation">WINNER</b>
+					<b class="scrollAnimation">Category: House of the Year / Emerging Designer of the Year </b>
+					<b class="scrollAnimation">Project: AZULIK Residence / Roth Architecture </b>
+					<span class="scrollAnimation"></span>
+					<p class="scrollAnimation">Established in 1997 and headquartered in Amsterdam, Frame is a global platform for all professional members and stakeholders in the interior-design industry. The Frame Awards honor outstanding architectural and design work around the world, always exploring what's next in spatial design, innovation, and creativity.</p>
+				</div>
+			</div>
+			<div class="awardImg scrollAnimation">
+				<img src="images/awards/5.jpg">
+				 <a href="https://frameweb.com/article/proudly-announcing-the-full-list-of-frame-awards-winners" target="_blank">Read More</a>
+				<div class="linea"></div>
+			</div>
+		</div>
+
+		<div class="awardItem">
+			<div class="contenido">
+				<div class="titulos">
+					<h3 class="scrollAnimation">LCD Berlin Awards</h3>
+					<h4 class="scrollAnimation">Berlin, Germany.</h4>
+				</div>
+				<div class="descripcion">
+					<b class="scrollAnimation">WINNER</b>
+					<b class="scrollAnimation">Category: New Culture Destination of the Year</b>
+					<b class="scrollAnimation">Project: SFER IK  Museion</b>
+					<span class="scrollAnimation"></span>
+					<p class="scrollAnimation">The Leading Culture Destinations Berlin Awards highlight the most remarkable, established, and emerging, cultural initiatives around the world – with nominees representing 50 cultural destinations from 28 countries. The “Oscars of museums” shine a light on the most exceptional offerings of the world’s cultural organizations.</p>
+				</div>
+			</div>
+			<div class="awardImg scrollAnimation">
+				<img src="images/awards/6.jpg">
+				 <a href="https://www.leadingculturedestinations.com/awards-1" target="_blank">Read More</a>
 				<div class="linea"></div>
 			</div>
 		</div>
