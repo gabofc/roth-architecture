feb54ccc8634d2c9b41c511ae925dab5960f7642
G@bo <gabofc@gmail.com>
2020-10-15 13:35:50 -0500

    About, Founder

--- v2/founder.php
+++ v2/founder.php
@@ -4,10 +4,11 @@
 		<h1 class="scrollAnimation">Roth</h1>
 		<h2 class="scrollAnimation">Founder</h2>
 		<div class="contentText">
-			<p class="scrollAnimation italica">“...una arquitectura que contemple las forma sustentables de vida que permiten a la naturaleza respirar. Necesitamos salir de la cueva conceptual y física, integrarnos a nuestro entorno natural.”</p>
-			<p class="scrollAnimation">Fundador de AZULIK, SFER IK y Enchanting Transformation, Roth es el líder creativo de Roth Architecture. Inspirado en las formas orgánicas y el balance ecosistémico su propuesta arquitectónica desafía las estructuras convencionales y convoca al juego, a la sorpresa y a la atención.</p>
-			<p class="scrollAnimation">Durante los últimos diez años Roth ha fomentado un diálogo continuo con las comunidades indígenas y contemporáneas para promover el intercambio de ideas y conocimientos. Con cada proyecto promueve  la preservación de la cultura local y la recuperación de los conocimientos y valores de la sabiduría ancestral para promover diferentes formas de interacción con el medio ambiente y su protección. </p>
-			<p class="scrollAnimation">Este esfuerzo converge con la preocupación de explorar, a través del arte, un reflejo de la esencia humana y la conexión con nuestro planeta.</p>
+			<p class="scrollAnimation italica">“…an architecture that considers sustainable forms of living that allow nature to breathe. We must go out of our conceptual and physical cave and reintegrate with our natural environs.”</p>
+			<p class="scrollAnimation">CEO and Founder of AZULIK and President of the Foundation Enchanting Transformation</p>
+			<p class="scrollAnimation">Founder of AZULIK, SFER IK, and Enchanting Transformation, Roth is the creative leader of Roth Architecture. Inspired by organic forms and ecosystem balance, his architectural proposal challenges conventional structures and calls for play, surprise, and attention.</p>
+			<p class="scrollAnimation">For the last ten years, Roth has fostered an ongoing dialogue with indigenous and contemporary communities to promote the exchange of ideas and knowledge. With each project, he promotes the preservation of local culture and the recovery of the knowledge and values ​​of ancestral wisdom to promote different forms of interaction with the environment and its protection.</p>
+			<p class="scrollAnimation">This effort converges with the urge to explore, through art, the conceptualization of the human essence and our connection with the planet.</p>
 		</div>
 	</div>
 	<img src="images/founder.jpg" class="founderImgTmp">
