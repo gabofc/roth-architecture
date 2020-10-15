9f357ebcb72a0e959d2971097e3b823e329c6236
G@bo <gabofc@gmail.com>
2020-10-15 08:53:49 -0500

    Press updates

--- v2/press.php
+++ v2/press.php
@@ -22,8 +22,8 @@
 			<!-- <span class="scrollAnimation">Date: January 2019</span>
 				<span class="scrollAnimation">Business Unit: AZULIK Uh May</span>-->
 			<h3 class="scrollAnimation">An Otherworldly Cultural Complex Grows Out of the Jungle in Mexico</h3>
-			<!-- <p class="scrollAnimation">Azulik Uh May—the brainchild of a Tulum-based hotelier—aims to be the most unusual and sought-after creative-residency program-cum-gallery on the planet.</p>-->
-			<a href="https://www.architecturaldigest.com/story/azulik-uh-may-tulum" class="scrollAnimation">Go to Link</a>
+			<p class="scrollAnimation">Under the 52-foot-tall dome of SFER IK, the central art gallery, live trees emerge from holes in the polished concrete floor and shoot straight through tunnels that hang down from the ceiling. The design is by an Argentine-born local hotel owner who simply goes by Roth, whose vision was to build a contemplative space where art convenes with nature.</p>
+			<a href="https://www.architecturaldigest.com/story/azulik-uh-may-tulum" class="scrollAnimation">Read More</a>
 		</div>
 	</div>
 </div>
@@ -37,7 +37,7 @@
 			<span class="scrollAnimation">Business Unit: AZULIK Tulum</span>-->
 			<h3 class="scrollAnimation">HOTEL AZULIK, un paraíso para perderse en la JUNGLA</h3>
 			<!--<p class="scrollAnimation">From above, it looks like a giant’s game of pickup sticks, with its explosion of vines, branches, and straw. At ground level, it soon becomes evident that this is a visionary’s kaleidoscopic creation, and a luxury one at that.</p>-->
-			<a href="https://www.revistaad.es/lugares/articulos/hotel-azulik-paraiso-para-perderse-jungla/22058" class="scrollAnimation">Go to Link</a>
+			<a href="https://www.revistaad.es/lugares/articulos/hotel-azulik-paraiso-para-perderse-jungla/22058" class="scrollAnimation">Read More</a>
 		</div>
 	</div>
 </div>
@@ -51,7 +51,7 @@
 			<span class="scrollAnimation">Business Unit: AZULIK Uh May</span>-->
 			<h3 class="scrollAnimation">Galería / Roth Architecture</h3>
 			<!--<p class="scrollAnimation">Azulik , un lugar del que ni siquiera hace falta salir una vez nos hemos registrado, porque ahí está todo lo que cualquier persona razonable podría necesitar.</p>-->
-			<a href="https://www.archdaily.mx/mx/896251/galeria-ik-lab-jorge-eduardo-neira-sterkel" class="scrollAnimation">Go to Link</a>
+			<a href="https://www.archdaily.mx/mx/896251/galeria-ik-lab-jorge-eduardo-neira-sterkel" class="scrollAnimation">Read More</a>
 		</div>
 	</div>
 </div>
@@ -60,13 +60,13 @@
 	<div class="principal">
 		<div class="pressImg scrollAnimation"><img src="images/press/decor.jpg" class="mainImg"></div>
 		<div class="pressContent">
-			<img src="images/press/decor.svg" class="scrollAnimation">
+			<img src="images/press/latitud_21.png" class="scrollAnimation">
 			<span class="scrollAnimation">Latitud 21</span>
 			<!-- <span class="scrollAnimation">Date: September 2019</span>
 			<span class="scrollAnimation">Business Unit: AZULIK Residence</span>-->
 			<h3 class="scrollAnimation">La revolución del espacio museístico en el nuevo milenio </h3>
 			<!--<p class="scrollAnimation">From above, it looks like a giant’s game of pickup sticks, with its explosion of vines, branches, and straw. At ground level, it soon becomes evident that this is a visionary’s kaleidoscopic creation, and a luxury one at that.</p>-->
-			<a href="https://drive.google.com/file/d/11JJTytKdf3XclXymcDQr-6mg5bvM6rLQ/view?usp=sharing" class="scrollAnimation">Go to Link</a>
+			<a href="https://drive.google.com/file/d/11JJTytKdf3XclXymcDQr-6mg5bvM6rLQ/view?usp=sharing" class="scrollAnimation">Read More</a>
 		</div>
 	</div>
 </div>
@@ -80,8 +80,9 @@
 			<!-- <span class="scrollAnimation">Date: September 2019</span>
 			<span class="scrollAnimation">Business Unit: SFER IK, Roth Architecture</span>-->
 			<h3 class="scrollAnimation">Redefining Life in the Jungle with a Non-Invasive Nature-Inspired Residence</h3>
-			<!--<p class="scrollAnimation">From above, it looks like a giant’s game of pickup sticks, with its explosion of vines, branches, and straw. At ground level, it soon becomes evident that this is a visionary’s kaleidoscopic creation, and a luxury one at that.</p>-->
-			<a href="https://l21.mx/roth-architecture-la-revolucion-del-espacio-museistico-en-el-nuevo-milenio/" class="scrollAnimation">Go to Link</a>
+			<p class="scrollAnimation">Roth Architecture crafted this unique residence to harmoniously blend in with the environment, respecting and honouring the natural habitat that hosts it. Centric to this award-winning design was the drive to redefine life in the jungle, promoting a non-invasive dialogue between architecture and its natural surroundings.
+</p>
+			<a href="https://l21.mx/roth-architecture-la-revolucion-del-espacio-museistico-en-el-nuevo-milenio/" class="scrollAnimation">Read More</a>
 		</div>
 	</div>
 </div>
@@ -90,13 +91,13 @@
 	<div class="principal">
 		<div class="pressImg scrollAnimation"><img src="images/press/decor.jpg" class="mainImg"></div>
 		<div class="pressContent">
-			<img src="images/press/decor.svg" class="scrollAnimation">
+			<img src="images/press/interior_design.png" class="scrollAnimation">
 			<span class="scrollAnimation"> ID Interior Design</span>
 			<!-- <span class="scrollAnimation">Date: October 2019</span>
 			<span class="scrollAnimation">Business Unit: Roth Architecture</span>-->
 			<h3 class="scrollAnimation">Sfer Ik Museion Uh May by Roth-Architecture: 2019 Best of Year Winner for Small Museum/Art Gallery</h3>
 			<!--<p class="scrollAnimation">From above, it looks like a giant’s game of pickup sticks, with its explosion of vines, branches, and straw. At ground level, it soon becomes evident that this is a visionary’s kaleidoscopic creation, and a luxury one at that.</p>-->
-			<a href="https://www.sbid.org/residential-design-uh-may-residence-19/" class="scrollAnimation">Go to Link</a>
+			<a href="https://www.sbid.org/residential-design-uh-may-residence-19/" class="scrollAnimation">Read More</a>
 		</div>
 	</div>
 </div>
