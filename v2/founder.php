d7685a5f053edccdf93e337dcccf8aa0e3d213d8
G@bo <gabofc@gmail.com>
2020-10-15 14:30:17 -0500

    Founder update

--- v2/founder.php
+++ v2/founder.php
@@ -4,7 +4,7 @@
 		<h1 class="scrollAnimation">Roth</h1>
 		<h2 class="scrollAnimation">Founder</h2>
 		<div class="contentText">
-			<p class="scrollAnimation italica">“…an architecture that considers sustainable forms of living that allow nature to breathe. We must go out of our conceptual and physical cave and reintegrate with our natural environs.”</p>
+			<p class="scrollAnimation italica">"…an architecture that considers sustainable ways of life that allow nature to breathe. We must go out of our conceptual and physical cave and reintegrate with our natural environs.”</p>
 			<p class="scrollAnimation">CEO and Founder of AZULIK and President of the Foundation Enchanting Transformation</p>
 			<p class="scrollAnimation">Founder of AZULIK, SFER IK, and Enchanting Transformation, Roth is the creative leader of Roth Architecture. Inspired by organic forms and ecosystem balance, his architectural proposal challenges conventional structures and calls for play, surprise, and attention.</p>
 			<p class="scrollAnimation">For the last ten years, Roth has fostered an ongoing dialogue with indigenous and contemporary communities to promote the exchange of ideas and knowledge. With each project, he promotes the preservation of local culture and the recovery of the knowledge and values ​​of ancestral wisdom to promote different forms of interaction with the environment and its protection.</p>
