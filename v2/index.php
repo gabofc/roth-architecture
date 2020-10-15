6773ef3b6011d6b56e5d53e15b3cd8e10833a08c
G@bo <gabofc@gmail.com>
2020-10-15 11:39:38 -0500

    Social links fix

--- v2/index.php
+++ v2/index.php
@@ -10,8 +10,8 @@
 ?>
 </ul>
 <div class="sociales">
-	<a href="https://twitter.com/Azulikofficial" target="_blank"><i class="fab fa-linkedin"></i></a>
-	<a href="https://www.instagram.com/azulikofficial/" target="_blank"><i class="fab fa-instagram"></i></a>
 	<a href="https://www.facebook.com/Azulikofficial/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+	<a href="https://www.instagram.com/azulikofficial/" target="_blank"><i class="fab fa-instagram"></i></a>
+	<a href="https://twitter.com/Azulikofficial" target="_blank"><i class="fab fa-twitter"></i></a>
 </div>
 <?php include 'footer.php'; ?>
