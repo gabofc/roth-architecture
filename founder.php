a6ffc0596df806f8b6e86867302d351b5159c341
Gabriel Fernandez <gfernandez@junkyard.mx>
2020-10-16 11:51:31 -0500

    adjust

--- founder.php
+++ founder.php
@@ -3,16 +3,18 @@
 	<div id="slideContainer">
 		<section class="panel blue">
 			<div class="principal">
-				<h1 class="scrollAnimation" id="mainSection">Roth</h1>
-				<h2 class="scrollAnimation">Founder</h2>
-				<div class="contentText">
-					<p class="scrollAnimation italica">"…an architecture that considers sustainable ways of life that allow nature to breathe. We must go out of our conceptual and physical cave and reintegrate with our natural environs.”</p>
-					<p class="scrollAnimation">CEO and Founder of AZULIK and President of the Foundation Enchanting Transformation</p>
-					<p class="scrollAnimation">Founder of AZULIK, SFER IK, and Enchanting Transformation, Roth is the creative leader of Roth Architecture. Inspired by organic forms and ecosystem balance, his architectural proposal challenges conventional structures and calls for play, surprise, and attention.</p>
-					<p class="scrollAnimation">For the last ten years, Roth has fostered an ongoing dialogue with indigenous and contemporary communities to promote the exchange of ideas and knowledge. With each project, he promotes the preservation of local culture and the recovery of the knowledge and values ​​of ancestral wisdom to promote different forms of interaction with the environment and its protection.</p>
-					<p class="scrollAnimation">This effort converges with the urge to explore, through art, the conceptualization of the human essence and our connection with the planet.</p>
-				</div>
 				<img src="images/founder/roth-01.jpg" class="founderImgTmp scrollAnimation">
+				<div class="pTop">
+					<h1 class="scrollAnimation" id="mainSection">Roth</h1>
+					<h2 class="scrollAnimation">Founder</h2>
+					<div class="contentText">
+						<p class="scrollAnimation italica">"…an architecture that considers sustainable ways of life that allow nature to breathe. We must go out of our conceptual and physical cave and reintegrate with our natural environs.”</p>
+						<p class="scrollAnimation">CEO and Founder of AZULIK and President of the Foundation Enchanting Transformation</p>
+						<p class="scrollAnimation">Founder of AZULIK, SFER IK, and Enchanting Transformation, Roth is the creative leader of Roth Architecture. Inspired by organic forms and ecosystem balance, his architectural proposal challenges conventional structures and calls for play, surprise, and attention.</p>
+						<p class="scrollAnimation">For the last ten years, Roth has fostered an ongoing dialogue with indigenous and contemporary communities to promote the exchange of ideas and knowledge. With each project, he promotes the preservation of local culture and the recovery of the knowledge and values ​​of ancestral wisdom to promote different forms of interaction with the environment and its protection.</p>
+						<p class="scrollAnimation">This effort converges with the urge to explore, through art, the conceptualization of the human essence and our connection with the planet.</p>
+					</div>
+				</div>
 			</div>
 		</section>
 		<section class="panel tCenter">
