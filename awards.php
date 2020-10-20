4e6e98b259c05145ff0a8656048c1f5471c1a584
G@bo <gabofc@gmail.com>
2020-10-20 13:27:58 -0500

    Awards update

--- awards.php
+++ awards.php
@@ -14,11 +14,11 @@
 					<b class="scrollAnimation">Category: Cultural Space</b>
 					<b class="scrollAnimation">Project: SFER IK Museion Uh May</b>
 					<span class="scrollAnimation"></span>
-					<p class="scrollAnimation">Idea-Tops aims to create the most thoughtful and influential space design award to discover and praise the best designers and design works around the world. With participants from over 50 countries, it is the most influential, international, and authoritative design award in China.</p>
+					<p class="scrollAnimation">Idea-Tops aims to create the most thoughtful and influential space design award to discover and praise the best designers and design works around the world. With participants of over 50 countries, it is the most influential, international, and authoritative design award in China.</p>
 				</div>
 			</div>
 			<div class="awardImg scrollAnimation">
-				<img src="images/awards/1.jpg">
+				<img src="images/awards/idea-tops.jpg">
 				 <a href="https://www.a963.com/index.php?m=idea_tops&c=ztshow&a=win_works_show_en&year=2019&pcat=1&cat=46" target="_blank">Read More</a>
 				<div class="linea"></div>
 			</div>
@@ -33,16 +33,32 @@
 						<b class="scrollAnimation">WINNER</b>
 						<b class="scrollAnimation">Categories: Details Plus Architecture + Wood</b>
 						<b class="scrollAnimation">Projects: SFER IK Museion Uh May (Jury Winner)</b>
-								<b class="scrollAnimation">WINNER</b>
-						<b class="scrollAnimation">Categories: Residential Private House (XL > 500 sq. ft.) </b>
-						<b class="scrollAnimation">Projects: AZULIK Residence (Special Mention)</b>
 					<span class="scrollAnimation"></span>
 					<p class="scrollAnimation">The Architizer A+Awards is the largest awards program focused on promoting and celebrating the year’s best architecture and products. The awards bring together the largest online community of architects in the world, including the elite of world architecture, who champion meaningful design and the qualities that are integral to bringing great buildings to life.</p>
 				</div>
 			</div>
 			<div class="awardImg scrollAnimation">
-				<img src="images/awards/2.jpg">
+				<img src="images/awards/architizer-01.jpg">
 				 <a href="https://architizer.com/projects/iklab-uh-may/?_ga=2.231018783.1274494379.1576182360-433655417.1576182360" target="_blank">Read More</a>
+				<div class="linea"></div>
+			</div>
+		</div>
+		<div class="awardItem">
+			<div class="contenido">
+				<div class="titulos">
+					<h3 class="scrollAnimation">ARCHITIZER A+ AWARDS 2019</h3>
+					<h4 class="scrollAnimation">N.Y., U.S.A.</h4>
+				</div>
+				<div class="descripcion">
+								<b class="scrollAnimation">WINNER</b>
+						<b class="scrollAnimation">Categories: Residential Private House (XL > 500 sq. ft.) </b>
+						<b class="scrollAnimation">Projects: AZULIK Residence (Special Mention)</b>
+					<span class="scrollAnimation"></span>
+				</div>
+			</div>
+			<div class="awardImg scrollAnimation">
+				<img src="images/awards/architizer-02.jpg">
+				 <a href="https://awards.architizer.com/winners-gallery/#ResidentialPrivateHouseXL5000sqft" target="_blank">Read More</a>
 				<div class="linea"></div>
 			</div>
 		</div>
@@ -61,7 +77,7 @@
 				</div>
 			</div>
 			<div class="awardImg scrollAnimation">
-				<img src="images/awards/3.jpg">
+				<img src="images/awards/sbid.jpg">
 				 <a href="https://www.sbid.org/residential-design-uh-may-residence-19/" target="_blank">Read More</a>
 				<div class="linea"></div>
 			</div>
@@ -81,7 +97,7 @@
 				</div>
 			</div>
 			<div class="awardImg scrollAnimation">
-				<img src="images/awards/4.jpg">
+				<img src="images/awards/idm-01.jpg">
 				 <a href="http://www.interiordesign.net/award/9-best-of-year-awards-winners-2019/#Small-Museum-Art-Gallery" target="_blank">Read More</a>
 				<div class="linea"></div>
 			</div>
@@ -97,16 +113,33 @@
 					<b class="scrollAnimation">WINNER</b>
 					<b class="scrollAnimation">Categories: House of the Year</b>
 					<b class="scrollAnimation">Projects: AZULIK Residence</b>
+					<span class="scrollAnimation"></span>
+					<p class="scrollAnimation">Established in 1997 and headquartered in Amsterdam, Frame is a global platform for all professional members and stakeholders in the interior-design industry. The Frame Awards honor outstanding architectural and design work around the world, always exploring what's next in spatial design, innovation and creativity.</p>
+				</div>
+			</div>
+			<div class="awardImg scrollAnimation">
+				<img src="images/awards/frame-awards-01.jpg">
+				 <a href="https://frameweb.com/article/proudly-announcing-the-full-list-of-frame-awards-winners" target="_blank">Read More</a>
+				<div class="linea"></div>
+			</div>
+		</div>
+
+		<div class="awardItem">
+			<div class="contenido">
+				<div class="titulos">
+					<h3 class="scrollAnimation">Frame Awards</h3>
+					<h4 class="scrollAnimation">Amsterdam, Netherlands.</h4>
+				</div>
+				<div class="descripcion">
 						<b class="scrollAnimation">WINNER</b>
 						<b class="scrollAnimation">Categories: Emerging Designer of the Year</b>
 						<b class="scrollAnimation">Projects: Roth Architecture </b>
 					<span class="scrollAnimation"></span>
-					<p class="scrollAnimation">Established in 1997 and headquartered in Amsterdam, Frame is a global platform for all professional members and stakeholders in the interior-design industry. The Frame Awards honor outstanding architectural and design work around the world, always exploring what's next in spatial design, innovation, and creativity.</p>
 				</div>
 			</div>
 			<div class="awardImg scrollAnimation">
-				<img src="images/awards/5.jpg">
-				 <a href="https://frameweb.com/article/proudly-announcing-the-full-list-of-frame-awards-winners" target="_blank">Read More</a>
+				<img src="images/awards/frame-awards-02.jpg">
+				 <a href="https://frameweb.com/article/proudly-announcing-the-full-list-of-frame-awards-winners " target="_blank">Read More</a>
 				<div class="linea"></div>
 			</div>
 		</div>
@@ -126,7 +159,7 @@
 				</div>
 			</div>
 			<div class="awardImg scrollAnimation">
-				<img src="images/awards/6.jpg">
+				<img src="images/awards/lcd.jpg">
 				 <a href="https://www.leadingculturedestinations.com/awards-1" target="_blank">Read More</a>
 				<div class="linea"></div>
 			</div>
@@ -143,16 +176,32 @@
 				<b class="scrollAnimation">WINNER</b>
 				<b class="scrollAnimation">Category: Best Artistic Architecture Studio </b>
 				<b class="scrollAnimation">Project: Roth Architecture</b>
+				<span class="scrollAnimation"></span>
+				<p class="scrollAnimation">BUILD Awards recognise and reward the works conducted by the world’s most innovative, imaginative and dedicated designers and architects. <br>With an impetus on sustainable and efficient methods being placed on modern projects, the awards recognise those that are pioneers in passive design. The Architecture Awards 2020 commend companies, individuals and projects across a myriad of sectors and through a merit-driven approach, that  ensures that only the most dedicated, creative and awe-inspiring parties receive one of the prestigious accolades.</p>
+			</div>
+		</div>
+		<div class="awardImg scrollAnimation">
+			<img src="images/awards/build-arch-01.jpg">
+			 <a href="https://www.build-review.com/issues/architecture-awards-2020/48/" target="_blank">Read More</a>
+			<div class="linea"></div>
+		</div>
+	</div>
+
+	<div class="awardItem">
+		<div class="contenido">
+			<div class="titulos">
+				<h3 class="scrollAnimation">BUILD ARCHITECTURE AWARDS 2020 </h3>
+				<h4 class="scrollAnimation">Staffordshire, England.</h4>
+			</div>
+			<div class="descripcion">
 					<b class="scrollAnimation">WINNER</b>
 					<b class="scrollAnimation">Category: Best Completed Innovative Commercial Project </b>
 					<b class="scrollAnimation">Project: ZAK IK</b>
 				<span class="scrollAnimation"></span>
-				<p class="scrollAnimation">BUILD Awards recognize and reward the works conducted by the world’s most innovative, imaginative, and dedicated designers and architects. With an impetus on sustainable and efficient methods being placed on modern projects, the awards acknowledge those companies and individuals who are pioneers in passive design.
-Winner</p>
 			</div>
 		</div>
 		<div class="awardImg scrollAnimation">
-			<img src="images/awards/6.jpg">
+			<img src="images/awards/build-arch-02.jpg">
 			 <a href="https://www.build-review.com/issues/architecture-awards-2020/48/" target="_blank">Read More</a>
 			<div class="linea"></div>
 		</div>
