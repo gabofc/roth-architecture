4370f5340d262505a5fe7647665832fb5c02457b
G@bo <gabofc@gmail.com>
2020-10-28 20:49:56 -0500

    Emails updated

--- contact-send.php
+++ contact-send.php
@@ -38,9 +38,9 @@
 		$mail->addCustomHeader( 'X-Mailer: ' . $form_values['host'] . '/ PHP/' . phpversion(), 'Message-ID: <' . gmdate( 'YmdHs' ) . '@' . $form_values['host'] . '/>', 'Sender: ' . $form_values['host'] . '/', 'Sent: ' . date( 'd-m-Y' ) );
 		$mail->From = 'crm@azulik.com';
 		$mail->FromName = 'Roth Architecture';
-		//$mail->AddAddress( 'contacto@roth-architecture.com', 'Roth Contact Form' );
+		$mail->AddAddress( 'contacto@roth-architecture.com', 'Roth Contact Form' );
 		$mail->AddAddress( 'gfernandez@azulik.com', 'Roth Contact Form' );
-		//$mail->AddAddress( 'fpires@azulik.com', 'Roth Contact Form' );
+		$mail->AddAddress( 'fpires@azulik.com', 'Roth Contact Form' );
 		$mail->IsHTML( true );
 		$mail->CharSet = 'UTF-8';
 		$mail->AltBody = $subject;
