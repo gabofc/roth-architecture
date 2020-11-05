2c9191db3bbe4db5c0c5b004fa183da87461c80e
G@bo <gabofc@gmail.com>
2020-11-05 11:26:52 -0500

    Odoo final integration

--- contact-send.php
+++ contact-send.php
@@ -35,8 +35,8 @@
 		$mail->addCustomHeader( 'X-Mailer: ' . $form_values['host'] . '/ PHP/' . phpversion(), 'Message-ID: <' . gmdate( 'YmdHs' ) . '@' . $form_values['host'] . '/>', 'Sender: ' . $form_values['host'] . '/', 'Sent: ' . date( 'd-m-Y' ) );
 		$mail->From = 'crm@azulik.com';
 		$mail->FromName = 'Roth Architecture';
-		//$mail->AddAddress( 'contacto@roth-architecture.com', 'Roth Contact Form' );
-		$mail->AddAddress( 'gfernandez@azulik.com', 'Roth Contact Form' );
+		$mail->AddAddress( 'contacto@roth-architecture.com', 'Roth Contact Form' );
+		//$mail->AddAddress( 'gfernandez@azulik.com', 'Roth Contact Form' );
 		//$mail->AddAddress( 'fpires@azulik.com', 'Roth Contact Form' );
 		$mail->IsHTML( true );
 		$mail->CharSet = 'UTF-8';
