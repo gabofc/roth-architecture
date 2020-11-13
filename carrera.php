a1cc46b63627554cce124688b3a30edc716205f7
Gabriel Fernandez <gfernandez@junkyard.mx>
2020-11-13 17:03:23 -0500

    adjust career detail page

--- carrera.php
+++ carrera.php
@@ -17,7 +17,7 @@
 	<div class="scrollAnimation parrafo">We are constantly looking for talented individuals to join our growing team and help us with various scale projects.</div>
 	<div class="scrollAnimation parrafo">Please find here all the career opportunities and positions we have available at the moment.</div>
 </div>
-<div class="shortMain carreraDetalle">
+<div class="shortMain carreraDetalle padCar">
 	<h3 class="scrollAnimation"><?php echo $carrera[ 'title' ]; ?></h3>
 	<span class="linea"></span>
 	<?php
@@ -41,9 +41,61 @@
 		}
 	?>
 	<?php endif; ?>
-	<?php if ( isset( $carrera[ 'apply' ] ) ) : ?>
 	<p class="scrollAnimation">To Apply:</p>
+	<?php if ( isset( $carrera[ 'apply' ] ) ) : ?>
 	<p class="scrollAnimation"><?php echo $carrera[ 'apply' ]; ?></p>
 	<?php endif; ?>
+	<form class="fila carreraForm" id="carreraForm" action="contact-send" method="POST">
+		<div class="fila">
+			<div class="campo">
+				<label for="email">Email</label>
+				<input type="text" id="email" name="email" required>
+			</div>
+		</div>
+		<div class="campo">
+			<label for="name">First Name</label>
+			<input type="text" id="name" name="name" required>
+		</div>
+		<div class="campo">
+			<label for="apellido">Last Name </label>
+			<input type="text" id="apellido" name="apellido" required>
+		</div>
+		<div class="campo">
+			<label for="country">Country</label>
+			<input type="text" id="country" name="country" required>
+		</div>
+		<div class="campo">
+			<label for="mobile">Mobile</label>
+			<input type="text" id="mobile" name="mobile" required>
+		</div>
+		<div class="campo">
+			<label for="salary">Current Salary</label>
+			<input type="text" id="salary" name="salary" required>
+		</div>
+		<div class="campo">
+			<label for="currency">Salary Currency</label>
+			<input type="text" id="currency" name="currency" required>
+		</div>
+		<div class="campo">
+			<label for="letter">Covering Letter</label>
+			<a href="#" class="campoDescarga">Select a file <i class="fas fa-download"></i></a>
+			<input type="file" id="letter" name="letter" required>
+			<span>File size should be no more than 8Mb</span>
+		</div>
+		<div class="campo">
+			<label for="curriculum">CV upload</label>
+			<a href="#" class="campoDescarga">Select a file <i class="fas fa-download"></i></a>
+			<input type="file" id="curriculum" name="curriculum" required>
+			<span>File size should be no more than 8Mb</span>
+		</div>
+		<div class="campo">
+			<label for="portfolio">Portfolio link </label>
+			<input type="text" id="portfolio" name="portfolio" required>
+			<span>You can upload your portfolio through a service like WeTransfer, Dropbox or Drive.</span>
+		</div>
+		<div class="full">
+			<input type="submit" value="Submit Application">
+		</div>
+	</form>
 </div>
 <?php include 'footer.php'; ?>
