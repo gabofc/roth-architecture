92abb34e56fa71dd23581cc005c91a207d617f6b
Gabriel Fernandez <gfernandez@junkyard.mx>
2020-11-13 08:57:19 -0500

    adjust services

--- services.php
+++ services.php
@@ -18,6 +18,56 @@
 				2 => 'Furthermore, we design integral experiences that go beyond the form or technique and through which people generate intimate bonds with their surroundings. Our spaces are not meant to be used, but felt.'
 			)
 		),
+		'architectural -design' => array(
+			'nombre' => 'Architectural Design',
+			'nombreVertical' => 'Architectural Design',
+			'imagen' => 'concept-experience.jpg',
+			'descripcion' => array(
+				1 => 'We engage with architectural design from a singular creative perspective. At the beginning of the process, we forgo established conventions in order to find unexpected solutions. We ask questions, explore ideas and develop proposals that grow from the location where a project is set, thus giving it a potent identity rooted in the land with which it coexists.',
+				2 => 'We take a project through every stage of design, from preliminary sketches and physical models to high-end photorealistic rendering, working closely with clients to co-create exceptional spaces.'
+			)
+		),
+		'executive-projects' => array(
+			'nombre' => 'Executive Projects',
+			'nombreVertical' => 'Executive Projects',
+			'imagen' => 'executive-projects.jpg',
+			'descripcion' => array(
+				1 => 'Our team works with an unsurpassed level of detail and delivers blueprints for any project to be executed seamlessly on the ground. We focus on capturing the intricacies of any concept to the highest detail, thus optimizing resources, streamlining communication and delivering specific architectural and engineering instructions that help articulate every part of the building process.'
+			)
+		),
+		'construction-management' => array(
+			'nombre' => 'Construction Management',
+			'nombreVertical' => 'Construction<br>Management',
+			'imagen' => 'construction-managenent.jpg',
+			'descripcion' => array(
+				1 => 'Our team of project managers can effectively oversee all aspects of the construction process. In addition to extensive know-how, we offer close attention to detail to ensure that the integrity of a project is preserved, while meeting local and international standards. We intimately direct the building process to guarantee that the oroginal idea is faithfully executed, and that all expectations of a space -whether functional, technical or conceptual- are satisfactorily met. '
+			)
+		),
+		'international-hospitality-development' => array(
+			'nombre' => 'International Hospitality Development',
+			'nombreVertical' => 'International<br>Hospitality Development',
+			'imagen' => 'international-hospitality.jpg',
+			'descripcion' => array(
+				1 => 'Given our long history of working in the hospitality industry in several parts of the world, we offer abundant knowledge on how to create cutting-edge, organic spaces that people will travel the world to see. We strive to construct not only spaces for rest, but new features of a destination that become central parts of the journey itself. Using local materials and capturing cultural and natural identities, we develop multi-sensory experiences that become landmarks in the most coveted destinations on the planet. '
+			)
+		),
+		'development' => array(
+			'nombre' => 'Development',
+			'nombreVertical' => 'Development',
+			'imagen' => 'concept-experience.jpg',
+			'descripcion' => array(
+				1 => 'We take projects from conception to completion. Our team can materialize any vision and take it from its earliest stages all the way to construction. We work on the ground to get closely acquainted with the land, getting to know the features of the primal natural design. Later, we put together a creative team and bring stakeholders that wish to take part in the co-creation process. Either for projects we conceive ourselves or ideas brought by a client, we gather the necessary resources, both material and intellectual, to bring them to life.'
+			)
+		),
+		'sustainable-design' => array(
+			'nombre' => 'Sustainable Design',
+			'nombreVertical' => 'Sustainable Design',
+			'imagen' => 'susteintable-design.jpg',
+			'descripcion' => array(
+				1 => 'Our buildings are intimately connected to the land. Our three basic pillars guide design that respects nature, preserves local knowledge and acts as a catalyst for creative exploration. Every project adapts to its environment to ensure the essential systems are enhanced by its presence, incorporating living nature to the spaces themselves or finding solutions to avoid degrading the ecosystem. ',
+				2 => 'Moreover, our structures become part of their surroundings. We understand that our presence is fleeting and that our creations must grow from the ground to, eventually, reintegrate to it as another component of the landscape.'
+			)
+		)
 	);
 ?>
 <div class="principal">
