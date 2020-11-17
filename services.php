4636ffab5ffb796e5aa97e76d7aadabb6572f598
Gabriel Fernandez <gfernandez@junkyard.mx>
2020-11-17 10:05:35 -0500

    adjust

--- services.php
+++ services.php
@@ -24,7 +24,7 @@
 			'<li class="servicioDetalle">
 				<div class="verticalTitle"><h2>0' . $i . ' ' . $servicio[ 'nombreVertical' ] . '</h2></div>
 				<div class="imageService"><img src="images/services/' . $servicio[ 'imagen' ] . '" alt="' . $servicio[ 'nombre' ] . '"></div>
-				<div class="servicioContent">
+				<div class="servicioContent ' . $servicio[ 'class' ] . '">
 					<h3><div class="fraccion"><span>0' . $i . '</span><span>0' . sizeof( $servicios ) . '</span></div> ' . $servicio[ 'nombre' ] . '</h3>
 					<span class="linea"></span>
 					' . $descripionText . '
