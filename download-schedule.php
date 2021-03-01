0ef171869d8652b7e437edd6abd9d37400dfb77a
Gabriel Fernandez <gfernandez@junkyard.mx>
2021-03-01 11:14:41 -0500

    with library

--- download-schedule.php
+++ download-schedule.php
@@ -1,41 +1,22 @@
 <?php
 if( isset($_REQUEST) && is_array($_REQUEST) ){
-  function format_date( $date ){
-    $formated_date = explode("/", $date);
-    return $formated_date[2].$formated_date[1].$formated_date[0];
-  }
-  function format_time( $time ){
-    $formated_time = explode(":", $time);
-    return $formated_time[0].$formated_time[1].'00';
-  }
-  $start_date = format_date($_REQUEST['dtstart']);
-  $start_time = format_time($_REQUEST['tstart']);
-  $end_time = format_time(date( "H:i", strtotime($start_time)+(60*30) ));
+  
 
-  function dateToCal($time) {
-    return date('Ymd\THis', strtotime($time)) . 'Z';
-  }
-
-  // Build the ics file
-  $ical = 'BEGIN:VCALENDAR
-  VERSION:2.0
-  PRODID:-//azulik/handcal//NONSGML v1.0//EN
-  CALSCALE:GREGORIAN
-  BEGIN:VEVENT
-  DTEND:' . dateToCal( $start_date.' '.$end_time ) . '
-  UID:' . md5($_REQUEST['summary']) . '
-  DTSTAMP:' . time() . '
-  LOCATION:' . addslashes('+'.$_REQUEST['location']) . '
-  DESCRIPTION:' . addslashes($_REQUEST['description']) . '
-  URL;VALUE=URI:
-  SUMMARY:' . addslashes($_REQUEST['summary']) . '
-  DTSTART:' . dateToCal( $start_date.' '.$start_time ) . '
-  END:VEVENT
-  END:VCALENDAR';
+  include 'ics.php';
 
   header('Content-Type: text/calendar; charset=utf-8');
-	header('Content-Disposition: attachment; filename=azulik-event.ics');
-	echo $ical;
+  header('Content-Disposition: attachment; filename=invite.ics');
+
+  $ics = new ICS(array(
+    'location' => $_REQUEST['location'],
+    'description' => $_REQUEST['description'],
+    'dtstart' => $_REQUEST['dtstart'] . ' ' . $_REQUEST['tstart'],
+    'summary' => $_REQUEST['summary']
+  ));
+
+echo $ics->to_string();
+
+
 } else {
   header('Location: /');
   exit();
