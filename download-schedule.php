5cedb31fb0f60b21082c15cd6ca50d5cf70ee0c5
G@bo <gabofc@gmail.com>
2021-03-01 11:15:30 -0500

    test

--- download-schedule.php
+++ download-schedule.php
@@ -1,5 +1,5 @@
 <?php
-if( isset($_REQUEST) && is_array($_REQUEST) ){
+/*if( isset($_REQUEST) && is_array($_REQUEST) ){
   function format_date( $date ){
     $formated_date = explode("/", $date);
     return $formated_date[2].$formated_date[1].$formated_date[0];
@@ -19,11 +19,11 @@ if( isset($_REQUEST) && is_array($_REQUEST) ){
   // Build the ics file
   $ical = 'BEGIN:VCALENDAR
   VERSION:2.0
-  PRODID:-//azulik/handcal//NONSGML v1.0//EN
+  PRODID:-//hacksw/handcal//NONSGML v1.0//EN
   CALSCALE:GREGORIAN
   BEGIN:VEVENT
   DTEND:' . dateToCal( $start_date.' '.$end_time ) . '
-  UID:' . md5($_REQUEST['summary']) . '
+  UID:' . md5( time() ) . '
   DTSTAMP:' . time() . '
   LOCATION:' . addslashes('+'.$_REQUEST['location']) . '
   DESCRIPTION:' . addslashes($_REQUEST['description']) . '
@@ -32,12 +32,51 @@ if( isset($_REQUEST) && is_array($_REQUEST) ){
   DTSTART:' . dateToCal( $start_date.' '.$start_time ) . '
   END:VEVENT
   END:VCALENDAR';
-
-  header('Content-Type: text/calendar; charset=utf-8');
+//var_dump($ical); exit();
+  header('Content-type: text/calendar; charset=utf-8');
 	header('Content-Disposition: attachment; filename=azulik-event.ics');
 	echo $ical;
+  exit();
 } else {
   header('Location: /');
+  exit();
+}*/
+?>
+
+<?php
+if( isset($_REQUEST) && is_array($_REQUEST) ){
+  include 'lib/ics.php';
+  function format_date( $date ){
+    $formated_date = explode("/", $date);
+    return $formated_date[2].$formated_date[1].$formated_date[0];
+  }
+  function format_time( $time ){
+    $formated_time = explode(":", $time);
+    return $formated_time[0].$formated_time[1].'00';
+  }
+  $start_date = format_date($_REQUEST['dtstart']);
+  $start_time = format_time($_REQUEST['tstart']);
+  $end_time = format_time(date( "H:i", strtotime($start_time)+(60*30) ));
+
+  function dateToCal($time) {
+    return date('Ymd\THis', strtotime($time)) . 'Z';
+  }
+
+  header('Content-Type: text/calendar; charset=utf-8');
+  header('Content-Disposition: attachment; filename=roth-architecture.ics');
+
+  $ics = new ICS(array(
+    'location' => '+'.$_REQUEST['location'],
+    'description' => addslashes($_REQUEST['description']),
+    'dtstart' => dateToCal( $start_date.' '.$start_time ),
+    'dtend' => dateToCal( $start_date.' '.$end_time ),
+    'summary' => addslashes($_REQUEST['summary']),
+    //'url' => $_REQUEST['url']
+  ));
+
+  echo $ics->to_string();
+} else {
+  header('Location: ./');
   exit();
 }
 ?>
